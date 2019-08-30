006da6663d8469e5c0b54115dd24886ef6f6974b
Deskola <dennis.ogunde@strathmore.edu>
2019-08-29 19:29:42 -0700

    Initial commit

---
+++ qrcode.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'vendor/autoload.php';
+$qrCode = new Endroid\QrCode\QrCode();
+
+$qrCode->setText('Life is too short to be generating QR codes');
+$qrCode->setSize(200);
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
+//$qrCode->setPadding(10);
+
+//$qrCode->render();
+
+// header('Content-Type: '.$qrCode->getContentType());
+// echo $qrCode->writeString();
+
+// require_once 'vendor/autoload.php';
+
+// $qr = new Endroid\Qrcode\QrCode();
+
+// $qr->setText('https://www.google.co.uk');
+// $qr->setSize(200);
+// $qr->setPadding(10);
+
+// $qr->render();
+
+
+?>
